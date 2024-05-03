15654933c216296255f9f03150cf4ef96333d2c0
kiko <kt1767@gmail.com>
2024-05-03 17:55:34 +0530

    Add react-ios-pwa-prompt for installing the app as a PWA on iOS

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import PWAPrompt from 'react-ios-pwa-prompt';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <App />
+    <PWAPrompt
+    promptOnVisit={1}
+    timesToShow={3}
+    copyClosePrompt="Close"
+    permanentlyHideOnDismiss={false}
+  />
   </React.StrictMode>
 );
 serviceWorkerRegistration.unregister();
